7791d6514dceb2b4c1c15bd28832f6d05955c13f
Najdorf <agustin.morro@gmail.com>
2025-01-11 20:58:05 -0300

    add googleMaps

---
+++ src/components/Maps/Places.jsx
@@ -0,0 +1,130 @@
+import { useState, useMemo, useRef, useEffect } from "react";
+import { useLoadScript } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+const libraries = ["places"];
+
+export default function Places({ userAddress, onLocationChange }) {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBf12D8PazAwJEEv91LJKc3G79zsUBl8pA",
+    libraries,
+    version: "weekly",
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Maps userAddress={userAddress} onLocationChange={onLocationChange} />;
+}
+
+function Maps({ userAddress, onLocationChange }) {
+  const center = useMemo(() => ({ lat: -32, lng: -60 }), []);
+  const [selected, setSelected] = useState({
+    lat: center.lat,
+    lng: center.lng,
+    address: userAddress || "",
+  });
+
+  useEffect(() => {
+    onLocationChange(selected);
+  }, [selected, onLocationChange]);
+
+  const handleDragEnd = async (event) => {
+    const position = event.latLng;
+    const lat = position.lat();
+    const lng = position.lng();
+
+    try {
+      const results = await getGeocode({ location: { lat, lng } });
+      const address = results[0]?.formatted_address || "Unknown location";
+      const newLocation = { lat, lng, address };
+      setSelected(newLocation);
+
+      onLocationChange(newLocation);
+    } catch (error) {
+      console.error("Error during reverse geocoding:", error);
+    }
+  };
+
+  return (
+    <div className="w-[400px] h-[100%] md:w-[600px]">
+      <PlacesAutocomplete setSelected={setSelected} selected={selected} />
+      <APIProvider apiKey={apiKey}>
+        <Map
+          mapId="DEMO_MAP_ID"
+          defaultCenter={center}
+          defaultZoom={12}
+          center={selected || center}
+          gestureHandling="greedy"
+          disableDefaultUI={true}
+        >
+          {selected && (
+            <AdvancedMarker
+              position={{ lat: selected.lat, lng: selected.lng }}
+              draggable={true}
+              onDragEnd={handleDragEnd}
+            />
+          )}
+        </Map>
+      </APIProvider>
+    </div>
+  );
+}
+
+const PlacesAutocomplete = ({ setSelected, selected }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  useEffect(() => {
+    if (selected.address) {
+      setValue(selected.address, false);
+    }
+  }, [selected.address, setValue]);
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = getLatLng(results[0]);
+      setSelected({ lat, lng, address });
+    } catch (error) {
+      console.error("Error getting geocode: ", error);
+    }
+  };
+
+  return (
+    <div className="autocomplete-container px-1 py-1 w-full lg:px-3 lg:pb-2">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        placeholder="Buscar"
+        className="autocomplete-input placeholder:text-zinc-400 my-1 focus:outline-none focus:ring-2 focus:ring-[#458552] focus:border-transparent"
+      />
+      {status === "OK" && (
+        <ul className="suggestions-list text-pretty px-2 py-2 flex flex-col gap-1 lg:py-3 lg:px-3 lg:text-lg rounded-b-lg ">
+          {data.map(({ place_id, description }) => (
+            <li
+              key={place_id}
+              onClick={() => handleSelect(description)}
+              className="text-stone-500 cursor-pointer"
+            >
+              - {description}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
