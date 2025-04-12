9a945a3fe1da5eb034f7aaaad55a024a46b95559
tablonimus <lautamartin1995@gmail.com>
2025-04-11 21:21:22 -0300

    chooseplace modal design

--- src/components/ChoosePlaceModal/ChoosePlaceModal.jsx
+++ src/components/ChoosePlaceModal/ChoosePlaceModal.jsx
@@ -71,7 +71,7 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
         aria-describedby="parent-modal-description"
         className=" flex items-center justify-center"
       >
-        <Box className="bg-[#fff] rounded-lg w-10/12 md:w-1/2 lg:w-1/3 overflow-y-auto   h-[80%] flex flex-col justify-between">
+        <Box className="bg-[#fff] rounded-lg w-[95%] max-w-[500px] h-[90vh] overflow-hidden flex flex-col justify-between shadow-lg">
           <section className=" p-4 ">
             <h2
               id="parent-modal-title"
@@ -79,13 +79,19 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
             >
               ¿Donde queres recibir tu pedido?
             </h2>
+            <p>
+              Envíos domicilio y puntos: Martes y Viernes entre las 9-14hs o las
+              15-20hs. Los pedidos que ingresan hasta las 12pm del día previo,
+              ingresan en el día previsto. Los que ingresan posterior a horario
+              se envian el próximo dia de entrega.
+            </p>
           </section>
-          <section className="flex px-5 py-5 gap-10">
+          <section className="flex flex-col w-full px-5 py-5 gap-6 md:gap-10 overflow-y-auto max-h-[calc(90vh-200px)]">
             <button
               className={
                 resume?.place?.type === "Envío a domicilio"
-                  ? "flex flex-col gap-5 items-center w-1/2 justify-center p-4  border-2 border-[#000] bg-[#81A165] rounded-lg text-white"
-                  : "flex flex-col gap-5 items-center w-1/2 justify-center p-4  border border-[#000] bg-[#fff]/50 rounded-lg hover:bg-[#81A165]"
+                  ? "flex flex-row gap-3 md:gap-5 items-center justify-start p-3 md:p-4 border-2 border-[#000] bg-[#81A165] rounded-lg text-white"
+                  : "flex flex-row gap-3 md:gap-5 items-center justify-start p-3 md:p-4 border border-[#000] bg-[#fff]/50 rounded-lg hover:bg-[#81A165]"
               }
               onClick={(e) =>
                 setResume({
@@ -94,20 +100,33 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
                 })
               }
             >
-              <MopedIcon
-                color="primary"
-                style={{ color: "#458552", height: "3em", width: "3em" }}
-              />
-              <span className="text-[14px] font-bold">Envío a domicilio</span>
-              <div className="flex flex-col">
-                {labels?.delivery_description}
+              <div className="w-8 md:w-1/12 flex-shrink-0">
+                <MopedIcon
+                  color={
+                    resume?.place?.type == "Envío a domicilio"
+                      ? "white"
+                      : "#458552"
+                  }
+                  className="h-8 w-8 md:h-12 md:w-12"
+                />
+              </div>
+              <div className="flex flex-col justify-start items-start">
+                <span className="text-sm md:text-[14px] font-bold text-start">
+                  Envío a domicilio
+                </span>
+                {resume?.place?.type == "Envío a domicilio" && (
+                  <p className="text-xs md:text-sm text-start">
+                    {labels?.delivery_description}
+                  </p>
+                )}
               </div>
             </button>
+
             <button
               className={
                 resume?.place?.type === "Retiro"
-                  ? "flex flex-col gap-5 items-center w-1/2 justify-center p-4  border-2 border-[#000] bg-[#81A165] rounded-lg text-white"
-                  : "flex flex-col gap-5 items-center w-1/2 justify-center p-4  border border-[#000] bg-[#fff]/50 rounded-lg hover:bg-[#81A165]"
+                  ? "flex flex-row gap-3 md:gap-5 items-center justify-start p-3 md:p-4 border-2 border-[#000] bg-[#81A165] rounded-lg text-white"
+                  : "flex flex-row gap-3 md:gap-5 items-center justify-start p-3 md:p-4 border border-[#000] bg-[#fff]/50 rounded-lg hover:bg-[#81A165]"
               }
               onClick={(e) =>
                 setResume({
@@ -116,21 +135,27 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
                 })
               }
             >
-              {/* <span className="text-[14px] text-yellow-500">Próximamente</span> */}
-              <StoreIcon
-                color="primary"
-                style={{ color: "#458552", height: "3em", width: "3em" }}
-              />
+              <div className="w-8 md:w-1/12 flex-shrink-0">
+                <StoreIcon
+                  color={resume?.place?.type == "Retiro" ? "white" : "#458552"}
+                  className="h-8 w-8 md:h-12 md:w-12"
+                />
+              </div>
 
-              <span className="text-[14px] font-bold">
-                Retiro en punto cercano
-              </span>
-              <div className="flex flex-col">
-                {labels?.pick_up_point_description}
+              <div className="flex flex-col justify-start items-start">
+                <span className="text-sm md:text-[14px] font-bold text-start">
+                  Retiro en punto cercano
+                </span>
+                {resume?.place?.type == "Retiro" && (
+                  <p className="text-xs md:text-sm text-start">
+                    {labels?.pick_up_point_description}
+                  </p>
+                )}
               </div>
             </button>
           </section>
-          <section className="flex flex-col items-center py-2">
+
+          <section className="flex flex-col items-center py-2 ">
             {resume?.place?.type === "Envío a domicilio" ? (
               <div className="flex flex-col items-center">
                 <span className="font-[500]">Seleccioná tu domicilio</span>
@@ -154,7 +179,7 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
                         <div className="flex items-center  gap-3 w-full">
                           <PlaceIcon
                             color="primary"
-                            sx={{ height: "1.3em", width: "1.3em" }}
+                            sx={{ height: "1.2em", width: "1.2em" }}
                           />
                           <div className="flex flex-col items-start">
                             <span className="text-sm">
@@ -186,55 +211,59 @@ export default function ChoosePlaceModal({ choosePlace, setChoosePlace }) {
                 <span className="font-[500]">
                   Seleccioná donde retirar tu pedido
                 </span>
-                {/* {console.log(pickupUsers)} */}
-                {pickupUsers?.length > 0
-                  ? pickupUsers?.map((pickup, index) => (
-                      <button
-                        key={index}
-                        className={
-                          resume.place.address === pickup.address
-                            ? " p-2 rounded-md  bg-[#81A165] border-2 border-[#000] hover:bg-[#81A165] text-white "
-                            : " p-2 rounded-md border border-gray-400 hover:bg-[#81A165] bg-[#fff]/60 "
-                        }
-                        onClick={(e) =>
-                          setResume({
-                            ...resume,
-                            ["place"]: {
-                              ...resume.place,
-                              address: pickup.address,
-                            },
-                          })
-                        }
-                      >
-                        <div className="flex items-center  gap-3 w-full">
-                          <PlaceIcon
-                            color="primary"
-                            sx={{ height: "1.3em", width: "1.3em" }}
-                          />
-                          <div className="flex flex-col items-start">
-                            <span className="text-sm">
-                              {pickup?.displayName}
-                            </span>
-                            <span className="text-sm opacity-80">
-                              {pickup.address?.name?.length < 80
-                                ? pickup.address.name
-                                : `${pickup.address.name.slice(0, 80)}...`}{" "}
-                              {pickup.address.number}
-                            </span>
-                            <span className="text-sm opacity-80">
-                              {pickup.address.locality}
-                            </span>
-                            <span className="text-sm opacity-80">
-                              {pickup.address.city}
-                            </span>
-                            {/* <span className="text-sm opacity-80">
+                <div className="flex flex-col justify-start p-2 gap-2 h-56 overflow-y-auto">
+                  {pickupUsers?.length > 0
+                    ? pickupUsers?.map((pickup, index) => (
+                        <button
+                          key={index}
+                          className={
+                            resume.place.address === pickup.address
+                              ? " p-2 rounded-md  bg-[#81A165] border-2 border-[#000] hover:bg-[#81A165] text-white "
+                              : " p-2 rounded-md border border-gray-400 hover:bg-[#81A165] bg-[#fff]/60 "
+                          }
+                          onClick={(e) =>
+                            setResume({
+                              ...resume,
+                              ["place"]: {
+                                ...resume.place,
+                                address: pickup.address,
+                              },
+                            })
+                          }
+                        >
+                          <div className="flex items-center  gap-3 w-full">
+                            <PlaceIcon
+                              color="primary"
+                              sx={{ height: "1.2em", width: "1.2em" }}
+                            />
+                            <div className="flex flex-col items-start">
+                              <span className="text-sm">
+                                {pickup?.displayName}
+                              </span>
+                              <span className="text-sm opacity-80">
+                                {pickup.address?.name?.length < 80
+                                  ? pickup.address.name
+                                  : `${pickup.address.name.slice(
+                                      0,
+                                      80
+                                    )}...`}{" "}
+                                {pickup.address.number}
+                              </span>
+                              <span className="text-sm opacity-80">
+                                {pickup.address.locality}
+                              </span>
+                              <span className="text-sm opacity-80">
+                                {pickup.address.city}
+                              </span>
+                              {/* <span className="text-sm opacity-80">
                               {pickup.address.tag}
                             </span> */}
+                            </div>
                           </div>
-                        </div>
-                      </button>
-                    ))
-                  : false}
+                        </button>
+                      ))
+                    : false}
+                </div>
               </div>
             ) : (
               false
