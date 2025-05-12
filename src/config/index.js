4a8a82bb67f9df9a32d21b9a4d81e28987083bb4
tablonimus <lautamartin1995@gmail.com>
2025-05-12 13:32:57 -0300

    V5.0.5

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const URL_PICTURES = "pictures";
 
 export class Settings {
   static get FRONTEND_VERSION() {
-    return "v5.4.0 (beta)"; //! Cambiar en cada despliegue.
+    return "v5.5.0 (beta)"; //! Cambiar en cada despliegue.
   }
 
   static get SERVER_URL() {
