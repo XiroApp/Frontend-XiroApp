67b84ee9dbf18b4200bd7728e2b8e1ac6240da4a
tablonimus <lautamartin1995@gmail.com>
2025-05-12 10:12:33 -0300

    mp release CSP

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const URL_PICTURES = "pictures";
 
 export class Settings {
   static get FRONTEND_VERSION() {
-    return "v5.1.0(vercel)"; //! Cambiar en cada despliegue.
+    return "v5.2.0 (beta)"; //! Cambiar en cada despliegue.
   }
 
   static get SERVER_URL() {
