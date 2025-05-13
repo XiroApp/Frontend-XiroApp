f4d026be84e9369330cedea594cc0d1ba9f817cc
tablonimus <lautamartin1995@gmail.com>
2025-05-13 20:34:57 -0300

    GIXI YIXI

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const URL_PICTURES = "pictures";
 
 export class Settings {
   static get FRONTEND_VERSION() {
-    return "v5.5.2 (beta)"; //! Cambiar en cada despliegue.
+    return "v6.0.0 (beta)"; //! Cambiar en cada despliegue.
   }
 
   static get SERVER_URL() {
