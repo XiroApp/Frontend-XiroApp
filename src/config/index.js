22736f4272c756abf1ca0e7f7498c910ade14269
tablonimus <lautamartin1995@gmail.com>
2025-05-23 16:58:59 -0300

    V6.0.1 beta

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ const URL_PICTURES = "pictures";
 
 export class Settings {
   static get FRONTEND_VERSION() {
-    return "v6.0.0 (beta)"; //! Cambiar en cada despliegue.
+    return "v6.0.1 (beta)"; //! Cambiar en cada despliegue.
   }
 
   static get SERVER_URL() {
