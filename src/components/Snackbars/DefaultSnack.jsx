f1b4bfe40b5d8af9bc13c9b9b3adf1750fd99b40
tablonimus <lautamartin1995@gmail.com>
2025-07-02 21:24:44 -0300

    last changes

--- src/components/Snackbars/DefaultSnack.jsx
+++ src/components/Snackbars/DefaultSnack.jsx
@@ -11,7 +11,7 @@ export default function DefaultSnack({ content = "" }) {
       className={
         open ? "flex justify-center items-center w-full px-4" : "hidden"
       }
-    >     
+    >
       <div className="flex gap-2 items-start w-full max-w-2xl justify-between bg-white font-bold  border-[1.5px] border-green-700 pl-4 pb-4 rounded-lg">
         <div className="w-full flex flex-col justify-start items-start gap-y-3 pt-4">
           <p className="text-3xl text-center w-full text-pretty">IMPORTANTE</p>
@@ -23,7 +23,8 @@ export default function DefaultSnack({ content = "" }) {
         </div>
         <button
           onClick={() => setOpen(false)}
-          className="p-1 mt-1 mr-1 text-black  hover:scale-105 transition-transform rounded-lg">
+          className="p-1 mt-1 mr-1 text-black  hover:scale-105 transition-transform rounded-lg"
+        >
           <svg
             fill="none"
             viewBox="0 0 24 24"
